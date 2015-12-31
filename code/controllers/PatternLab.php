fd6beb49c030acd76236600e918054a88591c678
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:58:40 +1300

    Converted to PSR-2

--- code/controllers/PatternLab.php
+++ code/controllers/PatternLab.php
@@ -3,48 +3,52 @@
 /**
  * @package patternlab
  */
-class PatternLab extends Page_Controller {
-    public function init() {
-		parent::init();
+class PatternLab extends Page_Controller
+{
+    public function init()
+    {
+        parent::init();
     }
 
-	public function index() {
+    public function index()
+    {
         Requirements::css(PATTERN_DIR . '/css/patternlab.css');
-		if(!Director::isDev() && !Permission::check('CMS_ACCESS_CMSMain')) {
-			return Security::permissionFailure($this);
-		}
+        if (!Director::isDev() && !Permission::check('CMS_ACCESS_CMSMain')) {
+            return Security::permissionFailure($this);
+        }
 
-		return $this->renderWith(array(
-			__CLASS__,
-			'Page'
-		));
-	}
+        return $this->renderWith(array(
+            __CLASS__,
+            'Page'
+        ));
+    }
 
-	public function getPatterns() {
+    public function getPatterns()
+    {
         $config = SiteConfig::current_site_config();
-		if ($config->Theme) {
-			Config::inst()->update('SSViewer', 'theme_enabled', true);
-			Config::inst()->update('SSViewer', 'theme', $config->Theme);
-		}
-		$theme = $config->Theme;
+        if ($config->Theme) {
+            Config::inst()->update('SSViewer', 'theme_enabled', true);
+            Config::inst()->update('SSViewer', 'theme', $config->Theme);
+        }
+        $theme = $config->Theme;
 
-		$manifest = SS_TemplateLoader::instance()->getManifest();
+        $manifest = SS_TemplateLoader::instance()->getManifest();
 
-		$templates = array();
+        $templates = array();
 
-		foreach($manifest->getTemplates() as $templateName => $templateInfo) {
-			$themeexists = $theme && isset($templateInfo['themes'][$theme]) && isset($templateInfo['themes'][$theme]['Patterns']);
+        foreach ($manifest->getTemplates() as $templateName => $templateInfo) {
+            $themeexists = $theme && isset($templateInfo['themes'][$theme]) && isset($templateInfo['themes'][$theme]['Patterns']);
 
-			if ((isset($templateInfo['Patterns']) || $themeexists) && !isset($templates[$templateName])) {
-				$templates[$templateName] = array(
+            if ((isset($templateInfo['Patterns']) || $themeexists) && !isset($templates[$templateName])) {
+                $templates[$templateName] = array(
                     'Name' => trim(preg_replace('/([A-Z])/', ' $1', preg_replace('/([0-9])/', '', $templateName))),
                     'Layout' => $this->renderWith(array($templateName))
                 );
-			}
-		}
+            }
+        }
 
-		// ksort($templates);
+        // ksort($templates);
 
-		return new ArrayList($templates);
-	}
+        return new ArrayList($templates);
+    }
 }
